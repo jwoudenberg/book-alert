36fa8ce9aa9c0bd06f91439b63f5b0fd45cf41b5
Jasper Woudenberg <mail@jasperwoudenberg.com>
2022-05-16 14:55:25 +0200

    Produce atom feed from wikidata queried info

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"encoding/xml"
 	"fmt"
 	"io"
 	"log"
@@ -13,21 +14,6 @@ import _ "embed"
 
 //go:embed recent-publications.sparql
 var query string
-
-type sparqlResults struct {
-	Results struct {
-		Bindings []struct {
-			Work            sparqlValue
-			AuthorLabel     sparqlValue
-			Title           sparqlValue
-			PublicationDate sparqlValue
-		}
-	}
-}
-
-type sparqlValue struct {
-	Value string
-}
 
 func main() {
 	client := &http.Client{}
@@ -58,12 +44,109 @@ func main() {
 		log.Println("Unexpected response:", resp.Status)
 		return
 	}
-	var data sparqlResults
-	err2 := json.Unmarshal(body, &data)
-	if err2 != nil {
-		log.Println(err2)
+	xml, err := rawSparqlToRawFeed(body)
+	if err != nil {
+		log.Println(err)
 		return
 	}
+	fmt.Println(string(xml))
+}
 
-	fmt.Printf("%+v\n", data.Results.Bindings)
+func rawSparqlToRawFeed(bytes []byte) ([]byte, error) {
+	sparql, err := sparqlFromJson(bytes)
+	if err != nil {
+		return nil, err
+	}
+	feed := sparqlToFeed(sparql)
+	xml, err := renderXml(feed)
+	if err != nil {
+		return nil, err
+	}
+	return xml, nil
+}
+
+func sparqlFromJson(bytes []byte) (sparqlResults, error) {
+	var sparql sparqlResults
+	err := json.Unmarshal(bytes, &sparql)
+	if err != nil {
+		return sparql, err
+	}
+	return sparql, nil
+}
+
+type sparqlResults struct {
+	Results struct {
+		Bindings []struct {
+			Work            sparqlValue
+			AuthorLabel     sparqlValue
+			Title           sparqlValue
+			PublicationDate sparqlValue
+		}
+	}
+}
+
+type sparqlValue struct {
+	Value string
+}
+
+type xmlFeed struct {
+	XMLName xml.Name `xml:"feed"`
+	Ns      string   `xml:"xmlns,attr"`
+	Id      string   `xml:"id"`
+	Title   string   `xml:"title"`
+	Link    xmlLink  `xml:"link"`
+	Updated string   `xml:"updated"`
+	Entries []xmlEntry
+}
+
+type xmlEntry struct {
+	XMLName xml.Name `xml:"entry"`
+	Title   string   `xml:"title"`
+	Link    xmlLink  `xml:"link"`
+	Id      string   `xml:"id"`
+	Updated string   `xml:"updated"`
+	Author  string   `xml:"author>name"`
+}
+
+type xmlLink struct {
+	XMLName xml.Name `xml:"link"`
+	Rel     string   `xml:"rel,attr"`
+	Href    string   `xml:"href,attr"`
+}
+
+func renderXml(feed xmlFeed) ([]byte, error) {
+	feedContents, err := xml.MarshalIndent(feed, "", "  ")
+	if err != nil {
+		return nil, err
+	}
+	xmlFeed := append([]byte(xml.Header), feedContents...)
+	return xmlFeed, nil
+}
+
+func sparqlToFeed(sparql sparqlResults) xmlFeed {
+	books := sparql.Results.Bindings
+	var updated string
+	if len(books) > 0 {
+		updated = books[0].PublicationDate.Value
+	} else {
+		updated = "1970-01-01T00:00:00Z"
+	}
+	feed := xmlFeed{
+		Ns:      "http://www.w3.org/2005/Atom",
+		Title:   "Book Alert",
+		Id:      "https://github.com/jwoudenberg/book-alert",
+		Link:    xmlLink{Rel: "self", Href: "https://github.com/jwoudenberg/book-alert"},
+		Updated: updated,
+	}
+	for _, book := range books {
+		entry := xmlEntry{
+			Title:   book.Title.Value,
+			Link:    xmlLink{Rel: "alternate", Href: book.Work.Value},
+			Id:      book.Work.Value,
+			Updated: book.PublicationDate.Value,
+			Author:  book.AuthorLabel.Value,
+		}
+		feed.Entries = append(feed.Entries, entry)
+	}
+	return feed
 }
--- main_test.go
+++ main_test.go
@@ -5,5 +5,85 @@ import (
 )
 
 func TestPlaceholder(t *testing.T) {
-	t.Errorf("No tests written yet.")
+	inputJson := `{
+  "head" : {
+    "vars" : [ "work", "authorLabel", "title", "publicationDate" ]
+  },
+  "results" : {
+    "bindings" : [ {
+      "work" : {
+        "type" : "uri",
+        "value" : "http://www.wikidata.org/entity/Q107123470"
+      },
+      "authorLabel" : {
+        "xml:lang" : "en",
+        "type" : "literal",
+        "value" : "Martha Wells"
+      },
+      "title" : {
+        "xml:lang" : "en",
+        "type" : "literal",
+        "value" : "Fugitive Telemetry"
+      },
+      "publicationDate" : {
+        "datatype" : "http://www.w3.org/2001/XMLSchema#dateTime",
+        "type" : "literal",
+        "value" : "2021-04-27T00:00:00Z"
+      }
+    }, {
+      "work" : {
+        "type" : "uri",
+        "value" : "http://www.wikidata.org/entity/Q100540380"
+      },
+      "authorLabel" : {
+        "xml:lang" : "en",
+        "type" : "literal",
+        "value" : "Martha Wells"
+      },
+      "title" : {
+        "xml:lang" : "en",
+        "type" : "literal",
+        "value" : "Network Effect"
+      },
+      "publicationDate" : {
+        "datatype" : "http://www.w3.org/2001/XMLSchema#dateTime",
+        "type" : "literal",
+        "value" : "2020-05-05T00:00:00Z"
+      }
+    } ]
+  }
+}`
+	expectedXml := `<?xml version="1.0" encoding="UTF-8"?>
+<feed xmlns="http://www.w3.org/2005/Atom">
+  <id>https://github.com/jwoudenberg/book-alert</id>
+  <title>Book Alert</title>
+  <link rel="self" href="https://github.com/jwoudenberg/book-alert"></link>
+  <updated>2021-04-27T00:00:00Z</updated>
+  <entry>
+    <title>Fugitive Telemetry</title>
+    <link rel="alternate" href="http://www.wikidata.org/entity/Q107123470"></link>
+    <id>http://www.wikidata.org/entity/Q107123470</id>
+    <updated>2021-04-27T00:00:00Z</updated>
+    <author>
+      <name>Martha Wells</name>
+    </author>
+  </entry>
+  <entry>
+    <title>Network Effect</title>
+    <link rel="alternate" href="http://www.wikidata.org/entity/Q100540380"></link>
+    <id>http://www.wikidata.org/entity/Q100540380</id>
+    <updated>2020-05-05T00:00:00Z</updated>
+    <author>
+      <name>Martha Wells</name>
+    </author>
+  </entry>
+</feed>`
+	actualXml, err := rawSparqlToRawFeed([]byte(inputJson))
+	if err != nil {
+		t.Errorf("Conversion failed: %s", err)
+	}
+	if string(actualXml) != expectedXml {
+		t.Errorf("Actual XML output did not match expected.\n%s", actualXml)
+	}
+
 }
