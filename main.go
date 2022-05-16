dba0bb5fcf12f49665df099a6346f0c0bd81b14f
Jasper Woudenberg <mail@jasperwoudenberg.com>
2022-05-16 11:33:57 +0200

    Query wikidata for recent publications
    The goal is to turn the data returned into an RSS feed and serve it. So
    far we only query wikidata for some hardcoded authors.

--- main.go
+++ main.go
@@ -2,8 +2,45 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
 )
 
+import _ "embed"
+
+//go:embed recent-publications.sparql
+var query string
+
 func main() {
-	fmt.Println("Hello, World!")
+	client := &http.Client{}
+	req, err := http.NewRequest(
+		"GET",
+		"https://query.wikidata.org/sparql?query="+url.QueryEscape(query),
+		nil,
+	)
+	// req.Header.Add("Content-Type", "application/sparql-query")
+	req.Header.Add("Accept", "application/sparql-results+json")
+	req.Header.Add("User-Agent", "book-alert/0.1.0 (github.com/jwoudenberg/book-alert); book-alert@jasperwoudenberg.com)")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	if resp.StatusCode != 200 {
+		log.Println("Unexpected response:", resp.Status)
+		return
+	}
+	fmt.Println(string(body))
 }
